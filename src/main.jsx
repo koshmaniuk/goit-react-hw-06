3f517429ee287354ce8518fd8104d1330af9c765
koshmaniuk <trident93@gmail.com>
2024-03-07 01:15:21 -0500

    folder name update

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./Components/App.jsx";
+import App from "./components/App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store.js";
